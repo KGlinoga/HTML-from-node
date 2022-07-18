e1926cdd7827673a8e3979991da231775eb774e0
Kristina Faye <kglinoga@gmail.com>
2022-07-18 10:24:23 -0700

    got to the same point as the challenge, with a function inside the .then value, but having trouble getting the data to print to the new file

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your name?',
+      name: 'name',
+    },
+    // {
+    //   type: 'input',
+    //   message: 'What is your location?',
+    //   name: 'location',
+    // },
+    // {
+    //   type: 'input',
+    //   message: 'Tell us about yourself:',
+    //   name: 'bio',
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'What is your LinkedIn URL?',
+    //     name: 'linkedIn',
+    //   },
+    //   {
+    //     type: 'input',
+    //     message: 'What is your GitHub URL?',
+    //     name: 'gitHub',
+    //   },
+  ])
+  .then((response) =>
+    response
+      ? userAnswers(response)
+      : console.log('You forgot your password already?!')
+  );
+
+  const userAnswers = (response) => {
+    console.log(response);
+    fs.writeFile("input.txt", response.name, (err) =>
+    err ? console.error(err) : console.log('Success!')
+  ); 
+  }
